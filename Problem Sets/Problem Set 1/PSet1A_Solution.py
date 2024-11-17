0ad177dc49cf070248ec75a50f0950b9aa9055f7
Pablo Silva <pbsilva2009@gmail.com>
2024-11-16 22:16:32 -0800

    Add files via upload

---
+++ Problem Sets/Problem Set 1/PSet1A_Solution.py
@@ -0,0 +1,36 @@
+## 6.100A PSet 1: Part A
+## Name: Pablo Silva
+## Time Spent: 00:21:15
+## Collaborators: N/A
+
+##################################################################################
+## Get user input for yearly_salary, portion_saved and cost_of_dream_home below ##
+##################################################################################
+
+yearly_salary = float(input('Enter your yearly salary: '))
+portion_saved = float(input('Enter the portion of your salary to save, as a decimal: '))
+cost_of_dream_house = float(input('Enter the cost of your dream house: '))
+portion_down_payment = 0.25 * cost_of_dream_house
+
+#########################################################################
+## Initialize other variables you need (if any) for your program below ##
+#########################################################################
+
+# Initializing the monthly salary makes it more convenient to calculate the number of months. - Pablo Silva
+monthly_salary = yearly_salary / 12
+amount_saved = 0
+r = 0.05
+months = 0
+
+###############################################################################################
+## Determine how many months it would take to get the down payment for your dream home below ## 
+###############################################################################################
+
+# This conditional statement ONLY works if your saved amount hasn't surpassed the cost of the house. - Pablo Silva
+while (amount_saved < portion_down_payment):
+    # Writting += achieves the same value as amount_saved == amount saved + x. - Pablo Silva
+    amount_saved += (portion_saved * monthly_salary + (amount_saved * (r / 12)))
+    # Make sure to increment your variable for every time it adds onto the amount saved. - Pablo Silva
+    months += 1
+    
+print(months)
