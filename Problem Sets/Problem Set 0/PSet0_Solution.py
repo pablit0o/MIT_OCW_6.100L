9a566f3393be95e285e52d159ea168d66544c6dd
Pablo Silva <pbsilva2009@gmail.com>
2024-11-16 22:14:49 -0800

    Add files via upload

---
+++ Problem Sets/Problem Set 0/PSet0_Solution.py
@@ -0,0 +1,15 @@
+## 6.100A PSet 0
+## Name: Pablo Silva
+## Time Spent: 00:00:30
+## Collaborators: N/A
+
+import numpy 
+
+x = 5
+y = 8
+
+# Remember to place the defined variables as part of the definition. - Pablo Silva
+z = x + y
+
+# Once again, place the defined variable as part of the definition. - Pablo Silva
+a = numpy.log2(z)
